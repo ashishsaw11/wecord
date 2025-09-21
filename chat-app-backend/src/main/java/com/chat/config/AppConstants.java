9023e1fd183db2439714a72aaa583392e1bbfcbe
ashishkrshaw <sawashishkumar327@gmail.com>
2025-09-21 22:11:53 +0530

    Initial commit: backend project

--- chat-app-backend/src/main/java/com/chat/config/AppConstants.java
+++ chat-app-backend/src/main/java/com/chat/config/AppConstants.java
@@ -1,6 +1,6 @@
 package com.chat.config;
 
 public class AppConstants {
-    public static final String FRONT_END_BASE_URL="http://localhost:5173";
+    public static final String FRONT_END_BASE_URL="https://wecord-s3vw.onrender.com";
 
 }
